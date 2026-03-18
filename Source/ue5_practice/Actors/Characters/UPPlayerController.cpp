20cb1083d77ebc4c5989942996f0cdf3d41c0a2f
MystIsle <pelhav@gmail.com>
2026-03-18 19:07:56 +0900

    04. 캐릭터 설정: PlayerController 코딩 : 입력 바인딩

--- Source/ue5_practice/Actors/Characters/UPPlayerController.cpp
+++ Source/ue5_practice/Actors/Characters/UPPlayerController.cpp
@@ -1,4 +1,53 @@
 ﻿// Copyright (c) 2026 Team Sparta. All rights reserved.
 
-
 #include "UPPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+void AUPPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (auto EnhancedInput = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInput->BindAction(InputAction_Move, ETriggerEvent::Triggered, this, &ThisClass::OnInputMoveTriggered);
+		EnhancedInput->BindAction(InputAction_Sprint, ETriggerEvent::Started, this, &ThisClass::OnInputSprint);
+		EnhancedInput->BindAction(InputAction_Sprint, ETriggerEvent::Completed, this, &ThisClass::OnInputSprint);
+		EnhancedInput->BindAction(InputAction_Jump, ETriggerEvent::Started, this, &ThisClass::OnInputJumpStarted);
+	}
+}
+
+void AUPPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (auto InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		InputSubsystem->AddMappingContext(InputMappingContext, 0);
+	}
+}
+
+void AUPPlayerController::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	if (auto InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		InputSubsystem->RemoveMappingContext(InputMappingContext);
+	}
+}
+
+void AUPPlayerController::OnInputMoveTriggered(const FInputActionValue& InputActionValue)
+{
+	
+}
+
+void AUPPlayerController::OnInputSprint(const FInputActionInstance& InputActionInstance)
+{
+
+}
+
+void AUPPlayerController::OnInputJumpStarted()
+{
+	
+}
--- Source/ue5_practice/Actors/Characters/UPPlayerController.h
+++ Source/ue5_practice/Actors/Characters/UPPlayerController.h
@@ -6,11 +6,39 @@
 #include "GameFramework/PlayerController.h"
 #include "UPPlayerController.generated.h"
 
-/**
- * 
- */
+class AUPCharacter;
+struct FInputActionValue;
+struct FInputActionInstance;
+class UInputMappingContext;
+class UInputAction;
+
 UCLASS()
 class UE5_PRACTICE_API AUPPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+	
+protected:
+	virtual void SetupInputComponent() override;
+	virtual void BeginPlay() override;
+	
+public:
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputMappingContext> InputMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputAction> InputAction_Move;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputAction> InputAction_Sprint;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputAction> InputAction_Jump;
+	
+private:
+	void OnInputMoveTriggered(const FInputActionValue& InputActionValue);
+	void OnInputSprint(const FInputActionInstance& InputActionInstance);
+	void OnInputJumpStarted();
 };
