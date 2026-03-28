8015480dc4319c2bf7eac1da517b9aaed367e260
MystIsle <pelhav@gmail.com>
2026-03-28 17:38:16 +0900

    09. 타격 구현 : AUPAIController : public AAIController 생성

---
+++ Source/ue5_practice/Actors/Characters/UPMonsterController.cpp
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2026 Team Sparta. All rights reserved.
+
+
+#include "UPMonsterController.h"
+
+
+AUPMonsterController::AUPMonsterController()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+FGenericTeamId AUPMonsterController::GetGenericTeamId() const
+{
+	if (IGenericTeamAgentInterface* TeamAgentInterface = Cast<IGenericTeamAgentInterface>(GetPawn()))
+	{
+		return TeamAgentInterface->GetGenericTeamId();
+	}
+	
+	return FGenericTeamId();
+}
+
---
+++ Source/ue5_practice/Actors/Characters/UPMonsterController.h
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2026 Team Sparta. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "UPMonsterController.generated.h"
+
+enum class EUPTeamID : uint8;
+
+UCLASS()
+class UE5_PRACTICE_API AUPMonsterController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	AUPMonsterController();
+
+	virtual FGenericTeamId GetGenericTeamId() const override;
+};
